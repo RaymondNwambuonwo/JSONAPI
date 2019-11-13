3394930eeba81b181e482ed1a51f5c4c893af315
RaymondNwambuonwo <Raymondnwambuonwogriffin@gmail.com>
2019-11-13 17:27:59 -0500

    created film schema and film seed

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const app = express();
 const parser = require("body-parser")
-
+const Characters = require("./lib/models/Characters")
 
 app.use(parser.json());
+
+app.get("/", function (req, res) {
+    // res.redirect("/Characters")
+    Characters.find({}).then(Characters => {
+        res.json(Characters);
+    })
+});
+
+
 
 
 app.listen(3000, () => console.log('listening on port 3000'));
